38bfe6fb7feb6be65d4ee97b7e9ffbf23b0621f4
telegrambot81 <telegrambot81@gmail.com>
2016-04-05 21:25:31 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -8,7 +8,7 @@
 do
 
 function run(msg, matches)
-  return '⚡️kingbot v1.0\n    \n  📢 Channel : @DarkTeam\n  👤 Admin : @AmirDark\n  \n  🙏 Special Thanks to :\n  \n        @PokerFace_Dev\n        @ShahabDark\n        @MR_Flat\n        @Sinaw1\n  \n Checkout yeo.ir/XamarinPlus\n' 
+  return '⚡️kingbot v1.0\n    \n  📢 Channe:...\n  👤 Admin : @AmirDark\n  \n  🙏 Special Thanks to :\n  \n        @PokerFace_Dev\n        @ShahabDark\n        @MR_Flat\n        @Sinaw1\n  \n Checkout yeo.ir/XamarinPlus\n' 
 end
 
 return {
